5e9bad713a3d18c55d03ba22c1b44bff957c8d18
Erick Pineda <erifabrizz07@gmail.com>
2026-05-17 19:25:32 +0200

    search bar and filters (only frontend)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,6 +26,39 @@ function App() {
 
       {error && <div className="alert alert-danger">{error}</div>}
 
+      <div className="mb-4">
+        <h3 className='text-center'>Looking for a specific study room?</h3>
+        <form className="d-flex">
+          <input className="form-control me-2" type="text" placeholder="Study Room Name" />
+          <button className="btn btn-primary" type="button">Search</button>
+        </form>
+      </div>
+
+      <div className='shadow-sm bg-light p-4 m-4 rounded '>
+        <h4 className='text-center'>Filters</h4>
+        <div className='row justify-content-center'>
+          <div className='col-md-4 mb-3 me-4'>
+            <label className='form-label'>Capacity</label>
+            <input type='number' className='form-control' placeholder='Minimum capacity' />
+          </div>
+          <div className='col-md-4 mb-3 ms-4'>
+            <label className='form-label'>Features</label>
+            <div className='form-check'>
+              <input className='form-check-input' type='checkbox' id='1' />
+              <label className='form-check-label' htmlFor='whiteboard'>Whiteboard</label>
+            </div>
+            <div className='form-check'>
+              <input className='form-check-input' type='checkbox' id='2' />
+              <label className='form-check-label' htmlFor='Video_Conferencing'>Video Conferencing</label>
+            </div>
+            <div className='form-check'>
+              <input className='form-check-input' type='checkbox' id='3' />
+              <label className='form-check-label' htmlFor='projector'>Projector</label>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="row">
         {rooms.map(room => (
           <div className="col-md-4 mb-4" key={room.RoomID}>
